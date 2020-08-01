147b0aff57d369f0646361d995325e6beb33ab50
maze@30022 <maze1234556@sjtu.edu.cn>
2020-08-01 13:35:58 +0800

    tmp

--- src_gail/train.sh
+++ src_gail/train.sh
@@ -6,4 +6,5 @@
 # CUDA_VISIBLE_DEVICES=7 mpirun -np 10 python3 gail_ppo_PID_unnorm_speed.py --config config/gail_ppo_biped_PID_unnorm_speed.yaml
 # CUDA_VISIBLE_DEVICES=9 mpirun -np 10 python3 gail_ppo_PID_multiClip_GP.py --config config/gail_ppo_biped_PID_multiClip.yaml
 # CUDA_VISIBLE_DEVICES=1 mpirun -np 10 python3 gail_ppo_PID_unnorm_variable_speed_graph.py --config config/gail_ppo_PID_unnorm_variable_speed_graph.yaml
-CUDA_VISIBLE_DEVICES=0 mpirun -np 10 python3 gail_ppo_PID_unnorm_graph.py --config config/gail_ppo_PID_unnorm_graph.yaml
+# CUDA_VISIBLE_DEVICES=0 mpirun -np 10 python3 gail_ppo_PID_unnorm_graph.py --config config/gail_ppo_PID_unnorm_graph.yaml
+CUDA_VISIBLE_DEVICES=0 mpirun -np 10 python3 gail_ppo_PID_unnorm_graph.py --config config/gail_ppo_PID_unnorm_graph-no_logits_reg.yaml
